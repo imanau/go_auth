881cccf8764540d263ef13c443bb7103c41f3e8a
imanau <ima_nau11@yahoo.co.jp>
2020-04-08 23:22:30 +0900

    Add echoでHellow Worldを実行
    標準パッケージであれほど苦労したjsonの返却が驚くほど簡単にできた

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import(
-	"fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
-func main(){
-	fmt.Println("Hello,World!!")
+func main() {
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/hello", mainPage())
+
+	// サーバー起動
+	e.Start(":3000") //ポート番号指定してね
+}
+
+func mainPage() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "Hello World")
+	}
 }
