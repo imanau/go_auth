7165c4484c40361539be0ea06a355364c444d627
imanau <ima_nau11@yahoo.co.jp>
2020-04-16 21:04:49 +0900

    Add Test SignupOk

--- go/interfaces/controllers/user_test.go
+++ go/interfaces/controllers/user_test.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo"
@@ -17,5 +18,20 @@ func TestIndex(t *testing.T) {
 	c.SetPath("/")
 	if assert.NoError(t, Index(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+// SignUpの正常系
+func TestSignupOk(t *testing.T) {
+	// param pattern
+	okJSON := `{"name":"ok","uid":"test1@example.com","password": "testpassdreafae","role": 1}`
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/sign_up", strings.NewReader(okJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/sign_up")
+	if assert.NoError(t, Signup(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
 	}
 }
