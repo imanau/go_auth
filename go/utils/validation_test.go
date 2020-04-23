ebb22329f16bae7a3deeb0a695059d1601274dbe
imanau <ima_nau11@yahoo.co.jp>
2020-04-23 22:20:09 +0900

    Add: バリデーションの単体テストを追加

--- go/utils/validation_test.go
+++ go/utils/validation_test.go
@@ -6,6 +6,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// EmailValidateのテスト
+func TestEmailValidate(t *testing.T) {
+	okuser := EmailValidation{Email: "test@example.com"}
+	nguser1 := EmailValidation{Email: "tesdaffadre"}
+	nguser2 := EmailValidation{Email: ""}
+	nguser3 := EmailValidation{Email: "@dafete.com"}
+	res1 := EmailValidate(okuser)
+	res2 := EmailValidate(nguser1)
+	res3 := EmailValidate(nguser2)
+	res4 := EmailValidate(nguser3)
+	assert.Equal(t, res1, true)
+	assert.Equal(t, res2, false)
+	assert.Equal(t, res3, false)
+	assert.Equal(t, res4, false)
+}
+
+// PasswordValidateのテスト
+func TestPasswordValidate(t *testing.T) {
+	okuser := PasswordValidation{Password: "password"}
+	nguser1 := PasswordValidation{Password: ""}
+	nguser2 := PasswordValidation{Password: "paaa"}
+	res1 := PasswordValidate(okuser)
+	res2 := PasswordValidate(nguser1)
+	res3 := PasswordValidate(nguser2)
+	assert.Equal(t, res1, true)
+	assert.Equal(t, res2, false)
+	assert.Equal(t, res3, false)
+}
+
 // UserValidateのテスト
 func TestUserValidate(t *testing.T) {
 	okuser := UserValidation{}
